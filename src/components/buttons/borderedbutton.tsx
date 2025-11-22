71d35900d7bb00679b440765e59058d4f94adaed
Mawhadmd <mohammedbus13@gmail.com>
2025-11-22 21:46:33 +0200

    feat: enhance EmailContact component with form validation, error handling, and improved layout; add subject selection and FAQ section

--- src/components/buttons/borderedbutton.tsx
+++ src/components/buttons/borderedbutton.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
-export default function BorderedButton({text, style}: {text: string, style?: string}) {
+export default function BorderedButton({
+  text,
+  style,
+}: {
+  text: string;
+  style?: string;
+}) {
   return (
-     <button  className={`w-fit min-w-44 h-12 border border-accent flex justify-center items-center rounded-lg text-xl text-accent font-bold ${style}`}>
-          {text}
-        </button>
-  )
+    <button
+      className={`w-fit min-w-44 h-12 border border-accent flex justify-center items-center rounded-lg text-xl text-accent font-bold ${style}`}
+    >
+      {text}
+    </button>
+  );
 }
